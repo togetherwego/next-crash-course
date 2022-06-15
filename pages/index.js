d472121567a84b008fcdc6834acaac317268b07c
firyalhz <iyaalfiryal@gmail.com>
2022-06-15 07:02:25 +0700

    add: data fetching, getStaticProps, showing data

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles)
   return (
     <div className={styles.container}>
       <Head>
         <title>Web Dev newz</title>
-        <meta name='keywords' content='web development, programming' />
+        <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome to next</h1>
+
+      <ArticleList articles={articles}/>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
