3794bb48fb78839f54f2746cc4b49e51ebbb09aa
firyalhz <iyaalfiryal@gmail.com>
2022-06-15 07:14:37 +0700

    add: getServerSideProps

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,10 +1,31 @@
 import { useRouter } from 'next/router'
+import Link from 'next/Link'
 
-const article = () => {
-  const router = useRouter()
-  const { id } = router.query
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
 
-  return <div>This is an Article {id}</div>
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  )
+}
+
+export const getServerSideProps = async context => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  )
+
+  const article = await res.json()
+  return {
+    props: {
+      article,
+    },
+  }
 }
 
 export default article
